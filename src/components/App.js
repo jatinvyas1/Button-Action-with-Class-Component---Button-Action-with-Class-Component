e0c8f5d0a3a3c305a5735cb3cb81c27b07ddaf5e
jatinvyas1 <jatin.vyas@outlook.com>
2021-03-11 21:01:09 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,31 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
-
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  constructor(props) {
+    super(props);
+    this.state({
+      showPara: false,
+    });
+  }
+  handleClick() {
+    this.setState({ showPara: true });
+  }
+  render() {
+    return (
+      <div id="main">
+        {this.state.showPara && (
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makes
+            me so happy
+          </p>
+        )}
+        <button id="button" onClick={this.handleClick}>
+          button
+        </button>
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
